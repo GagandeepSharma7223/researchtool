5dfb7c6902f2004e84182c73e066ffee02be2d09
Cton System <sharmag674@gmail.com>
2021-12-16 23:26:53 +0530

    Update mongodb.driver version and add logging to timeline controller

--- chapterone.researchlibrary/Controllers/TimelineController.cs
+++ chapterone.researchlibrary/Controllers/TimelineController.cs
@@ -1,5 +1,6 @@
 ﻿using chapterone.data.interfaces;
 using chapterone.data.models;
+using chapterone.services.interfaces;
 using chapterone.web.viewmodels;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,14 +18,15 @@ namespace chapterone.web.controllers
         private const int TIMELINE_QUERY_PAGESIZE = 20;
 
         private readonly IDatabaseRepository<Message> _timelineRepo;
-
+        private readonly IEventLogger _logger;
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public TimelineController(IDatabaseRepository<Message> timelineRepo)
+        public TimelineController(IDatabaseRepository<Message> timelineRepo, IEventLogger logger)
         {
             _timelineRepo = timelineRepo;
+            _logger = logger;
         }
 
 
@@ -32,17 +34,25 @@ namespace chapterone.web.controllers
         [HttpGet("/timeline")]
         public async Task<IActionResult> GetTimelineView([FromQuery] int page = 0)
         {
-            var timeline = await _timelineRepo.QueryAsync(x => true, x => x.Created, isAscending: false, pageNumber: page, pageSize: TIMELINE_QUERY_PAGESIZE);
-            var maxCount = await _timelineRepo.CountAsync(x => true);
-
-            var vm = new TimelineViewModel()
+            try
             {
-                Entries = timeline.OrderByDescending(x => x.Created.LocalDateTime),
-                CurrentPage = page,
-                MaxPages = (int)maxCount / 20
-            };
+                var timeline = await _timelineRepo.QueryAsync(x => true, x => x.Created, isAscending: false, pageNumber: page, pageSize: TIMELINE_QUERY_PAGESIZE);
+                var maxCount = await _timelineRepo.CountAsync(x => true);
 
-            return View("~/views/Timeline.cshtml", vm);
+                var vm = new TimelineViewModel()
+                {
+                    Entries = timeline.OrderByDescending(x => x.Created.LocalDateTime),
+                    CurrentPage = page,
+                    MaxPages = (int)maxCount / 20
+                };
+
+                return View("~/views/Timeline.cshtml", vm);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogException(ex);
+                return NotFound();
+            }
         }
     }
 }
